afd63ce16fd14916ed895e6f40d1add3e73c748f
Lucas Faria <up202207540@up.pt>
2024-03-18 19:55:27 +0000

    added the authentication service class

---
+++ src/green_go/lib/model/user_model.dart
@@ -0,0 +1,12 @@
+
+class UserModel{
+  String username = "";
+  String uid = "";
+  int points = 0;
+
+  String password = "";
+  String email = "";
+
+  //contructor
+  UserModel(this.uid, this.username, this.points, this.password, this.email);
+}
