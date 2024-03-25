50ab10911886abef47ea83d70f7ee2b62142061d
Lucas Faria <up202207540@up.pt>
2024-03-25 11:15:51 +0000

    Added the location functionality to the trip page

--- src/green_go/lib/view/pages/trip_page.dart
+++ src/green_go/lib/view/pages/trip_page.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:green_go/controller/location/location.dart';
 import 'package:green_go/view/pages/take_picture_screen.dart';
 import 'package:green_go/view/widgets/menu_bar.dart';
 
-class TripPage extends StatelessWidget{
+class TripPage extends StatefulWidget{
   const TripPage({super.key});
-  
+
+  @override
+  _TripPage createState() => _TripPage();
+}
+
+class _TripPage extends State<TripPage> {
+  Future<Position>? location;
+
+  void showLocation(){
+    setState(() {
+      LocationService().requestLocationPermission();
+      location = LocationService().determinePosition();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(
+          const Padding(
+            padding: EdgeInsets.all(35),
+            child: Text(
+              "Trip Selection",
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+           ),
+           const Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "This page will contain a selection of sustainable transports to choose and start getting points. For now you can try the system that will be used to verify the trips and calculate the points (the pictures and the location).",
+              style: TextStyle(
+                color: Color.fromARGB(100, 239, 0, 0),
+              ) ,) ,
+           ),
+          Padding(
+            padding: const EdgeInsets.all(35),
             child: Center(
               child: ElevatedButton(
                 onPressed: (){
@@ -22,12 +55,37 @@ class TripPage extends StatelessWidget{
                       ),
                 );
               },
-              child: const Text("TakePicture"),),
+              child: const Text("Take a Picture"),),
             ),
           ),
-          const CustomMenuBar(),
+          Padding(
+            padding: const EdgeInsets.all(35),
+            child: Center(
+              child: ElevatedButton(
+                    onPressed: (){
+                      showLocation();
+                    },
+                    child: const Text("Show Location"),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(35),
+            child: FutureBuilder(
+              future: location, 
+              builder:(context, snapshot) {
+                if(snapshot.hasData){
+                  return Text(snapshot.data.toString());
+                }
+                else{
+                  return const Text("No location");
+                }
+              }
+                ),
+          ),   
         ],
       ),
+      bottomSheet: const CustomMenuBar(),
     );
   }
   
