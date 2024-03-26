51e7e13944cb5ab822cf9cdfaac3812b4c4f37b2
Lucas Faria <up202207540@up.pt>
2024-03-26 22:04:07 +0000

    Updates to the readme

--- README.md
+++ README.md
@@ -139,12 +139,93 @@ The camera and the GPS is for the validation of his mission. For example, if he 
 
 
 #### Vertical prototype
+In the vertical prototype we implemented the following features:
+
+* **Login Page:** 
+  - **User Story:** As a user that has already signed up I want to login in my account so that I can access it and the app's contents.
+  - **Implementation:** In the prototype, this feature is basically completed. Only some minor visual enhancements could be made for the final version.
+  - **Screenshot:** 
+    
+    <img src="docs/images/Prototype/Login.jpg" alt="Sequence Diagram" style="width:300px">
+
+* **Register Page:**
+  - **User Story:** As a new user I want to sign up for the app so that i can participate in the challenges.
+  - **Implementation:** This feature is basically completed. Only some minor visual enhancements could be made for the final version.
+  - **Screenchot:**
+
+   <img src="docs/images/Prototype/Register.jpg" alt="Sequence Diagram" style="width:300px">
+
+* **Start Page:** 
+  - **User Story:** As a new user I want to sign up for the app so that i can participate in the challenges. As a user that has already signed up I want to login in my account so that I can access it and the app's contents.
+   - **Implementation:** This feature is basically completed (we can access the login and register). Only some minor visual enhancements could be made for the final version.
+  - **Screenshot:**
+
+   <img src="docs/images/Prototype/Start_Page.jpg" alt="Sequence Diagram" style="width:300px">
+
+* **Leaderboard:**
+  - **User Story**: As a user I want to check the leaderboard so that i can see my current position in it along with the points of other users.
+  - **Implementation:** The system already connects with the Firebase Firestore database and fetches the users data. However, the users aren't ordered by the number of points and the visuals are different from what we expect for the final version.
+  - **Screenshot:** 
+
+  <img src="docs/images/Prototype/Leaderboard.jpg" alt="Sequence Diagram" style="width:300px">
+
+* **Trip Page:**
+  - **User Story:** As a user I want to check a trip using public transports
+so that i can receive points.
+  - **Implementation:** In this prototype, this page is only used to demonstrate the technologies that will be used to verify the trips (GPS location and camera pictures).
+  - **Screenshot:** 
+
+  <img src="docs/images/Prototype/trip_page.jpg" alt="Sequence Diagram" style="width:300px">
+
+* **Menu Bar**
+  - **User Story:** As a user I want to have a menu so that I can go see my profile, the leaderboard, all the missions, the main page and the bus page.
+  - **Implementation:** The menu bar is fully functional but the visuals aren't what we expect for the final version.
+  - **Screenshot:**
+
+  <img src="docs/images/Prototype/Menu_bar.jpg" alt="Sequence Diagram" style="width:300px">
+
+* **Search Missions page**
+  - **User Story:** As a user I want to be able to search for specifics missions/mission types to be able to more effectively gain points
+  - **Implementation:** The page only has the title and the menu bar already enables the user to access this page. However it doesn't have any of the main features implemented.
+  - **Screenshot:**
+
+  <img src="docs/images/Prototype/Missions.jpg" alt="Sequence Diagram" style="width:300px">
+
+
+
+* **Profile Page:**
+  - **User Story:** 
+  - **Implementation:** The page only has the title and the menu bar already enables the user to access this page. However it doesn't have any of the main features implemented.
+  - **Screenshot:**
+
+<img src="docs/images/Prototype/Profile.jpg" alt="Sequence Diagram" style="width:300px">
+
+
+* **Main Page:**
+  - **User Story:** As a user I want to have a main page so that I can check out my score, streak and how many more points I need to complete my current goal as well as a few missions that are available to be completed.
+  - **Implementation:** The page only has the app logo and the menu bar already enables the user to access this page. However it doesn't have any of the main features implemented.
+  - **Screenshot:**
+
+  <img src="docs/images/Prototype/Main_Page.jpg" alt="Sequence Diagram" style="width:300px">
+
+
+
+
+
+
+
+
+
+
+
 
 To help on validating all the architectural, design and technological decisions made, we usually implement a vertical prototype, a thin vertical slice of the system integrating as much technologies we can.
 
 In this subsection please describe which feature, or part of it, you have implemented, and how, together with a snapshot of the user interface, if applicable.
 
 At this phase, instead of a complete user story, you can simply implement a small part of a feature that demonstrates they you can use the technology, for example, show a screen with the app credits (name and authors).
+
+
 
 ## Project management
 
--- src/green_go/lib/view/pages/register_page.dart
+++ src/green_go/lib/view/pages/register_page.dart
@@ -26,187 +26,189 @@ class RegisterPageViewState extends State<RegisterPage>{
   Widget build(BuildContext context){
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(35),
-        child: Column(
-          children: [
-            Image.asset('images/GreenGo.png'),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Register",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
+        padding: const EdgeInsets.only(top: 35, left: 35, right: 35),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('images/GreenGo.png'),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
                 ),
               ),
-            ),
-            const Padding(padding: EdgeInsets.all(10)),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Name:",
-                style: TextStyle(
-                  fontSize: 20,
+              const Padding(padding: EdgeInsets.all(10)),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Name:",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            TextFormField(
-              textAlign: TextAlign.center,
-                controller: usernameController,
+              const Padding(padding: EdgeInsets.all(5)),
+              TextFormField(
+                textAlign: TextAlign.center,
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                    ),
+                  ),
+              ),
+              const Padding(padding: EdgeInsets.all(15)),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "E-mail:",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(5)),
+              TextFormField(
+                textAlign: TextAlign.center,
+                controller: emailController,
                 decoration: InputDecoration(
                   contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0)
                   ),
                 ),
-            ),
-            const Padding(padding: EdgeInsets.all(15)),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "E-mail:",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
               ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            TextFormField(
-              textAlign: TextAlign.center,
-              controller: emailController,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(15)),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Password:",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            TextFormField(
-              textAlign: TextAlign.center,
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(15)),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Confirm password:",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            TextFormField(
-              textAlign: TextAlign.center,
-              controller: confPasswordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(20)),
-            RichText(
-              text: TextSpan(
-                style: DefaultTextStyle.of(context).style,
-                children: <TextSpan>[
-                  const TextSpan(
-                      text: 'Already have an account? Login ',
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.black,
-                        decoration: TextDecoration.none,
-                      ),
-                  ),
-                  TextSpan(
-                    text: 'Here',
-                    style: const TextStyle(
-                      color: Colors.blue,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.blue,
-                      fontSize: 10,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const LoginPage()),
-                        );
-                      },
-                  ),
-                ],
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            ElevatedButton(
-              style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(const Size(100, 60)),
-                backgroundColor: MaterialStateProperty.all(lightGreen),
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-              ),
-              onPressed: () async {
-                String username = usernameController.text.trim();
-                String email = emailController.text.trim();
-                String password = passwordController.text;
-                String confirmPassword = confPasswordController.text;
-                if (password != confirmPassword) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Passwords do not match.'),
-                    ),
-                  );
-                  return;
-                }
-                await authService.signUp(email, password)
-                    .then((signUpResult) {
-                  if (signUpResult == null) {
-                    // Registration successful, navigate to login page
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => const LoginPage()),
-                    );
-                    User? user = authService.getCurrentUserID();
-                    String? uid = user?.uid;
-                    dataBaseUsers.addUser(uid!, username);
-                  } else {
-                    // Registration failed, show error message
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(signUpResult),
-                      ),
-                    );
-                  }
-                });
-              },
-              child: const Text(
-                  'Register',
+              const Padding(padding: EdgeInsets.all(15)),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Password:",
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 20,
                   ),
+                ),
               ),
-            ),
-          ],
+              const Padding(padding: EdgeInsets.all(5)),
+              TextFormField(
+                textAlign: TextAlign.center,
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(15)),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Confirm password:",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(5)),
+              TextFormField(
+                textAlign: TextAlign.center,
+                controller: confPasswordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(20)),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    const TextSpan(
+                        text: 'Already have an account? Login ',
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),
+                    ),
+                    TextSpan(
+                      text: 'Here',
+                      style: const TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.blue,
+                        fontSize: 10,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const LoginPage()),
+                          );
+                        },
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(5)),
+              ElevatedButton(
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(const Size(100, 60)),
+                  backgroundColor: MaterialStateProperty.all(lightGreen),
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+                onPressed: () async {
+                  String username = usernameController.text.trim();
+                  String email = emailController.text.trim();
+                  String password = passwordController.text;
+                  String confirmPassword = confPasswordController.text;
+                  if (password != confirmPassword) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Passwords do not match.'),
+                      ),
+                    );
+                    return;
+                  }
+                  await authService.signUp(email, password)
+                      .then((signUpResult) {
+                    if (signUpResult == null) {
+                      // Registration successful, navigate to login page
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                      );
+                      User? user = authService.getCurrentUserID();
+                      String? uid = user?.uid;
+                      dataBaseUsers.addUser(uid!, username);
+                    } else {
+                      // Registration failed, show error message
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(signUpResult),
+                        ),
+                      );
+                    }
+                  });
+                },
+                child: const Text(
+                    'Register',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
