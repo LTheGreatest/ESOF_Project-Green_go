70187cc5626d14fe82f3b0c88d9445dbc1dbc4f4
Lucas Faria <up202207540@up.pt>
2024-04-11 09:52:30 +0100

    Added the firebase cloud storage service

---
+++ src/green_go/lib/controller/database/cloud_storage.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+
+class CloudStorage{
+  final storageRef = FirebaseStorage.instance.ref();
+
+  UploadTask uploadFile(File file, String path){
+    return storageRef.child(path).putFile(file);
+  }
+
+  Future<String> dowloadFileURL(String path) async{
+    return await storageRef.child(path).getDownloadURL();
+  }
+}
