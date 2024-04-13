f60ee38848bc0cf1369c52cb591779b906662e38
Manuel Mo <manuelmo.steam@gmail.com>
2024-04-13 23:34:37 +0100

    some unit tests done

--- src/green_go/test/lib/firebase_options_test.dart
+++ src/green_go/test/lib/firebase_options_test.dart
@@ -1,8 +1,39 @@
+import 'package:flutter/foundation.dart';
 import 'package:green_go/firebase_options.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+
 void main() {
-  test('test example', () {
-  expect(0, 1);
+  group('DefaultFirebaseOptions', () {
+    setUpAll(() {
+      TestWidgetsFlutterBinding.ensureInitialized();
+    });
+    test('web', () {
+      if (kIsWeb) expect(DefaultFirebaseOptions.currentPlatform, DefaultFirebaseOptions.web);
+    });
+    test('Android', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.android;
+      expect(DefaultFirebaseOptions.currentPlatform, DefaultFirebaseOptions.android);
+    });
+    test('iOS', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+      expect(DefaultFirebaseOptions.currentPlatform, DefaultFirebaseOptions.ios);
+    });
+    test('macOS', () {;
+      debugDefaultTargetPlatformOverride = TargetPlatform.macOS;
+      expect(DefaultFirebaseOptions.currentPlatform, DefaultFirebaseOptions.macos);
+    });
+    test('windows', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.windows;
+      expect(() => DefaultFirebaseOptions.currentPlatform, throwsA(isA<UnsupportedError>()));
+    });
+    test('linux', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.linux;
+      expect(() => DefaultFirebaseOptions.currentPlatform, throwsA(isA<UnsupportedError>()));
+    });
+    test('fuchsia', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+      expect(() => DefaultFirebaseOptions.currentPlatform, throwsA(isA<UnsupportedError>()));
+    });
   });
 }
