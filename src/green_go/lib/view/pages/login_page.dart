6d4fd4fb8e4418ebbbd23597722f382f836177c9
Lucas Faria <up202207540@up.pt>
2024-04-15 12:03:28 +0100

    Improved the login page organization

--- src/green_go/lib/view/pages/login_page.dart
+++ src/green_go/lib/view/pages/login_page.dart
@@ -17,6 +17,132 @@ class LoginPageViewState extends State<LoginPage>{
   final TextEditingController passwordController = TextEditingController();
   final AuthService authService = AuthService();
 
+  Widget loginText(BuildContext context){
+    return const Text(
+          "Login",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        );
+  }
+
+  Widget labelText(BuildContext context, String text){
+    //label of the input forms
+    return Text(
+            text,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          );
+  }
+
+  Widget inputForm(BuildContext context, TextEditingController controller, bool obscure ){
+    //Where the user enter it's credentials
+    return TextFormField(
+        textAlign: TextAlign.center,
+        controller: controller,
+        obscureText: obscure ,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0)
+          ),
+        ),
+      );
+  }
+
+  Widget dontHaveAccountText(BuildContext context){
+    //Text tha informs the user that he needs to register before login
+    return RichText(
+
+          text: TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+              const TextSpan(
+                text: 'Don’t have an account? Register ',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+
+              TextSpan(
+                text: 'Here',
+                style: const TextStyle(
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.blue,
+                  fontSize: 10,
+                ),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const RegisterPage()),
+                    );
+                  },
+              ),
+            ],
+          ),
+        );
+  }
+
+  Widget loginButton(BuildContext context){
+    //button to login in the account
+    return ElevatedButton(
+
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(const Size(100, 60)),
+                backgroundColor: MaterialStateProperty.all(lightGreen),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+
+              onPressed: () async {
+                //calls the authentication service to sign in the user
+                String? signInResult = await authService.signIn(
+                  emailController.text,
+                  passwordController.text,
+                );
+
+                //Verifies if the context is mounted in order to continue
+                if (!context.mounted) return;
+
+                //verifies the sign in result and performs the necessary actions.
+                if (signInResult == 'Successfully logged in') {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MainPage())
+                  );
+
+                } else if (signInResult == 'User not found: Double check your email') {
+                  emailController.clear();
+
+                } else if (signInResult == 'Wrong password, try again'){
+                  passwordController.clear();
+
+                } else {
+                  emailController.clear();
+                  passwordController.clear();
+                }
+
+                //message that appears on the screen with the sign in result
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(signInResult!),
+                  ),
+                );
+                },
+              
+              //Button text
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            );
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -24,129 +150,41 @@ class LoginPageViewState extends State<LoginPage>{
         padding: const EdgeInsets.all(35),
         child: Column(
           children: [
+
+            //app logo
             Image.asset('images/GreenGo.png'),
-            const Align(
+
+            //login text
+            Align(
               alignment: Alignment.topLeft,
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                ),
-              ),
+              child: loginText(context),
             ),
+
             const Padding(padding: EdgeInsets.all(10)),
-            const Align(
+
+            //email text and input text area
+            Align(
               alignment: Alignment.topLeft,
-              child: Text(
-                "E-mail:",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
+              child: labelText(context, "E-mail:"),
             ),
             const Padding(padding: EdgeInsets.all(5)),
-            TextFormField(
-              textAlign: TextAlign.center,
-              controller: emailController,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-              ),
-            ),
+            inputForm(context, emailController, false),
+
+            //password text and input text area
             const Padding(padding: EdgeInsets.all(15)),
-            const Align(
+            Align(
               alignment: Alignment.topLeft,
-              child: Text(
-                "Password:",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
+              child: labelText(context, "Password:"),
             ),
             const Padding(padding: EdgeInsets.all(5)),
-            TextFormField(
-              textAlign: TextAlign.center,
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-              ),
-            ),
+            inputForm(context, passwordController, true),
+
+            //don't have account text
             const Padding(padding: EdgeInsets.all(50)),
-            RichText(
-              text: TextSpan(
-                style: DefaultTextStyle.of(context).style,
-                children: <TextSpan>[
-                  const TextSpan(
-                    text: 'Don’t have an account? Register ',
-                    style: TextStyle(
-                      fontSize: 10,
-                      color: Colors.black,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'Here',
-                    style: const TextStyle(
-                      color: Colors.blue,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.blue,
-                      fontSize: 10,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const RegisterPage()),
-                        );
-                      },
-                  ),
-                ],
-              ),
-            ),
+            dontHaveAccountText(context),
             const Padding(padding: EdgeInsets.all(5)),
-            ElevatedButton(
-              style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(const Size(100, 60)),
-                backgroundColor: MaterialStateProperty.all(lightGreen),
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-              ),
-              onPressed: () async {
-                String? signInResult = await authService.signIn(
-                  emailController.text,
-                  passwordController.text,
-                );
-                if (!mounted) return; // Add this line to check if the widget is still in the tree
-                if (signInResult == 'Successfully logged in') {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MainPage())
-                  );
-                } else if (signInResult == 'User not found: Double check your email') {
-                  emailController.clear();
-                } else if (signInResult == 'Wrong password, try again'){
-                  passwordController.clear();
-                } else {
-                  emailController.clear();
-                  passwordController.clear();
-                }
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(signInResult!),
-                  ),
-                );
-                },
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
+
+            loginButton(context),
           ],
         ),
       ),
