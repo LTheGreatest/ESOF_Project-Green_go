cf896dd48767b8a46ff1c8a52a3de2553866d7fc
Lucas Faria <up202207540@up.pt>
2024-04-16 22:11:18 +0100

    Added some tests for the icons fetcher

--- src/green_go/test/controller/fetchers/transports_fetcher_test.dart
+++ src/green_go/test/controller/fetchers/transports_fetcher_test.dart
@@ -1,5 +1,3 @@
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 
 import 'package:green_go/controller/database/database_transports.dart';
@@ -14,6 +12,7 @@ import 'transports_fetcher_test.mocks.dart';
 void main(){
 
   test("get transports", () async{
+    //given
     TransportsFetcher fetcher = TransportsFetcher();
     DataBaseTransports db = MockDataBaseTransports();
     final instance =  FakeFirebaseFirestore();
@@ -27,13 +26,13 @@ void main(){
       'pointsPerDist': 2.0
     }
     );
-
     when(db.getAllData()).thenAnswer((realInvocation) => instance.collection('transports').get());
-
     fetcher.setDB(db);
 
+    //when
     final transports = await fetcher.getTransports();
 
+    //then
     expectLater(transports[0].name, 'bus');
     expectLater(transports[0].pointsPerDist, 1.5);
     expectLater(transports[1].name, 'train');
---
+++ src/green_go/test/controller/fetchers/user_score_fetcher_test.dart
@@ -0,0 +1,11 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:green_go/controller/database/database_users.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+@GenerateNiceMocks([MockSpec<DataBaseUsers>()])
+
+void main(){
+
+}
