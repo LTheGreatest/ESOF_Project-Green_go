fa991d21a855735fe540a1ab7ea8abe2ec0e658b
Lucas Faria <up202207540@up.pt>
2024-04-22 10:52:11 +0100

    Improved the visuals of the search page

--- src/green_go/lib/view/pages/search_page.dart
+++ src/green_go/lib/view/pages/search_page.dart
@@ -32,6 +32,70 @@ class SearchPageState extends State<SearchPage>{
   bool containsString(String title){
     return title.toLowerCase().contains(searchString.toLowerCase());
   }
+  
+  Widget searchTextField(BuildContext context){
+    return TextField(
+          textAlign: TextAlign.start,
+          controller: searchController,
+          obscureText: false,
+          decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0)
+            ),
+            prefixIcon: const Padding(
+              padding: EdgeInsets.only(left:20, right: 20),
+              child: Icon(Icons.search),
+            ),
+            prefixIconConstraints: const BoxConstraints(),
+            hintText: "Search",
+            
+          ),
+          onChanged: searchMissions,
+        );
+  }
+
+  Widget missionContainer(BuildContext context, MissionsModel missionsModel){
+    return Container(
+              width: MediaQuery.of(context).size.width * 0.3,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2
+                ),
+                borderRadius: const BorderRadius.all(Radius.elliptical(20, 20)),
+              ),
+      
+              child: TextButton(
+                onPressed: (){
+                },
+      
+                child: Text(
+                  missionsModel.title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600
+                  )
+                ),
+              ),
+            );
+  }
+
+  Widget missionsList(BuildContext context, List<MissionsModel> missions){
+    return Expanded(
+      child: ListView.builder(
+        itemCount: missions.length,
+        itemBuilder: (BuildContext context, int index){
+          return containsString(missions[index].title)? 
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: missionContainer(context, missions[index]),
+          ) :
+          const Padding(padding: EdgeInsets.zero);
+        } 
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,64 +104,14 @@ class SearchPageState extends State<SearchPage>{
             children: [
               Padding(
                 padding: const EdgeInsets.fromLTRB(20,100,20,20),
-                child: TextField(
-                            textAlign: TextAlign.start,
-                            controller: searchController,
-                            obscureText: false,
-                            decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10.0)
-                              ),
-                              prefixIcon: const Padding(
-                                padding: EdgeInsets.only(left:20, right: 20),
-                                child: Icon(Icons.search),
-                              ),
-                              prefixIconConstraints: const BoxConstraints(),
-                              hintText: "Search",
-                              
-                            ),
-                            onChanged: searchMissions,
-                          ),
+                child: searchTextField(context),
               ),
           FutureBuilder(
             future: missionsFuture,
             builder: (context, snapshot) {
               if(snapshot.hasData){
                 missions = snapshot.data!;
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: missions.length,
-                    itemBuilder: (BuildContext context, int index){
-                      return containsString(missions[index].title)? 
-                          Container(
-                          width: MediaQuery.of(context).size.width * 0.3,
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              width: 2
-                            ),
-                            borderRadius: const BorderRadius.all(Radius.elliptical(20, 20)),
-                          ),
-                  
-                          child: TextButton(
-                            onPressed: (){
-                            },
-                  
-                            child: Text(
-                              missions![index].title,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600
-                              )
-                            ),
-                          ),
-                      ) :
-                      Padding(padding: EdgeInsets.zero);
-                  
-                    } 
-                  ),
-                );
+                return missionsList(context, missions);
               }
               else{
                  return const Center(
@@ -108,7 +122,7 @@ class SearchPageState extends State<SearchPage>{
           ),
         ],
       ),
-      bottomSheet: CustomMenuBar(),
+      bottomSheet: const CustomMenuBar(),
     );
   }
 
