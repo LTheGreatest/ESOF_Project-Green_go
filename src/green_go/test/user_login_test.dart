0e54684814cf94a6e00fe81e1e21debd4fb57567
Manuel Mo <manuelmo.steam@gmail.com>
2024-04-26 16:18:23 +0100

    Made divided file for login test.

---
+++ src/green_go/test/user_login_test.dart
@@ -0,0 +1,69 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:green_go/controller/authentication/auth.dart';
+import 'package:green_go/model/user_model.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'auth_test.mocks.dart';
+import 'package:green_go/controller/database/database_users.dart';
+
+
+class MockUserCredential extends Mock implements UserCredential {}
+class MockDataBaseUsers extends Mock implements DataBaseUsers {
+  @override
+  Future addUser(UserModel user) {
+    return Future.value();
+  }
+}
+class MockUser extends Mock implements User {
+  @override
+  String get uid => '123';
+}
+
+
+@GenerateNiceMocks([MockSpec<FirebaseAuth>()])
+void main() {
+  late MockFirebaseAuth firebaseAuth;
+  late AuthService authService;
+  late MockDataBaseUsers mockDataBaseUsers;
+
+  setUp(() {
+    firebaseAuth = MockFirebaseAuth();
+    authService = AuthService();
+    mockDataBaseUsers = MockDataBaseUsers();
+    authService.setFirebaseAuth(firebaseAuth);
+    authService.setDataBaseUsers(mockDataBaseUsers);
+  });
+
+  group('Sign in - ', () {
+    test('Successful', () async {
+      when(firebaseAuth.signInWithEmailAndPassword(
+        email: 'test@example.com',
+        password: '123456',
+      )).thenAnswer((_) async => MockUserCredential()); // MockUserCredential should be defined or use a real UserCredential instance
+
+      final result = await authService.signIn('test@example.com', '123456');
+      expect(result, equals("Successfully logged in"));
+    });
+
+    test('User not found', () async {
+      when(firebaseAuth.signInWithEmailAndPassword(
+        email: 'wrong@example.com',
+        password: 'incorrect',
+      )).thenThrow(FirebaseAuthException(code: 'user-not-found'));
+
+      final result = await authService.signIn('wrong@example.com', 'incorrect');
+      expect(result, equals("User not found: Double check your email"));
+    });
+
+    test('Wrong password', () async {
+      when(firebaseAuth.signInWithEmailAndPassword(
+        email: 'test@example.com',
+        password: 'wrongpassword',
+      )).thenThrow(FirebaseAuthException(code: 'wrong-password'));
+
+      final result = await authService.signIn('test@example.com', 'wrongpassword');
+      expect(result, equals("Wrong password, try again"));
+    });
+  });
+}
