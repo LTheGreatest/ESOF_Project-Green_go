8f1251e94ac668c44aab9e7fde37ac4a2ed27345
Alexandre Lopes <alexandrefernadeslopes@gmail.com>
2024-05-17 19:09:06 +0100

    changed mission history page to be closer to achievements page and better

--- src/green_go/lib/view/pages/mission_history_page.dart
+++ src/green_go/lib/view/pages/mission_history_page.dart
@@ -75,42 +75,49 @@ class MissionHistoryState extends State<MissionHistoryPage > {
   Widget backButtonAndTitle(BuildContext context){
     //return the back button and the title
     return Stack(
-            children: [
-              //back button
-              Align(
-                alignment: Alignment.centerLeft,
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        PageRouteBuilder(
-                          pageBuilder: (context, animation, secondaryAnimation) =>  const ProfilePage(),
-                          transitionDuration: Duration.zero,
-                          reverseTransitionDuration: Duration.zero,
-                        ));
-                  },
-                  icon: const Icon(Icons.arrow_back, size: 40),
+      children: [
+        //back button
+        Align(
+          alignment: Alignment.centerLeft,
+          child: IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) => const ProfilePage(),
+                  transitionDuration: Duration.zero,
+                  reverseTransitionDuration: Duration.zero,
                 ),
-              ),
-              //title
-              const Padding(
-                padding: EdgeInsets.only(left: 35, top: 5),
-                child: Align(
-                    alignment: Alignment.center,
-                    child: Text("Completed Missions",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold
-                      ),
-                    )
+              );
+            },
+            icon: const Icon(Icons.arrow_back, size: 40),
+          ),
+        ),
+        //title
+        const Padding(
+            padding: EdgeInsets.only(left: 35, top: 5),
+            child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Mission History",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold
+                  ),
                 )
-              ),
-            ],
-          );
+            )
+        ),
+      ],
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: backButtonAndTitle(context),
+      ),
       body: FutureBuilder(
           future: Future.wait([getCompletedMissions()]),
           builder: (context, snapshot) {
@@ -123,29 +130,38 @@ class MissionHistoryState extends State<MissionHistoryPage > {
                       width: MediaQuery.of(context).size.width * 0.88,
                       height: MediaQuery.of(context).size.height * 0.82,
                       decoration: BoxDecoration(
-                          border: Border.all(
-                              width: 1
-                          ),
-                          borderRadius: const BorderRadius.all(Radius.elliptical(20, 20))
+                        border: Border.all(width: 1),
+                        borderRadius: const BorderRadius.all(
+                          Radius.elliptical(20, 20),
+                        ),
                       ),
                       child: Column(
                         children:[
-                          Padding(padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02)),
-                          //Back button and title
-                          backButtonAndTitle(context),
-                          Padding(padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.04)),
-                          //List of completed missions
+                          Padding(
+                            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10, top: 5), // Adjusted top padding
+                            child: Text(
+                              "Missions Completed",
+                              style: TextStyle(
+                                fontSize: 22, // Increased font size
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
                           Expanded(
-                              child: ListView.builder(
-                                  padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-                                  itemCount: completedMissions.length,
-                                  itemBuilder: (context, index) {
-                                    return Padding(
-                                        padding: const EdgeInsets.only(top: 15),
-                                        child: missionContainer(context, index),
-                                    );
-                                  })
-                          )
+                            child: ListView.builder(
+                                padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                                itemCount: completedMissions.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(top: 15),
+                                    child: missionContainer(context, index),
+                                  );
+                                }
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -157,7 +173,8 @@ class MissionHistoryState extends State<MissionHistoryPage > {
                 child: CircularProgressIndicator(),
               );
             }
-          }),
+          }
+      ),
       bottomNavigationBar: const CustomMenuBar(currentPage: MenuPage.profile,),
     );
   }
